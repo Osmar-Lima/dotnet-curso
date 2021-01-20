ab7905c20de29e5d186556131e94b89d472d41c0
Osmar Lima <osmar.minhos@gmail.com>
2021-01-20 18:33:05 -0300

    Atualizando o projeto

---
+++ DigitalInnovationOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; // pacote do proprio dotnet
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 5;
+            string nome= "Osmar";
+            string curso = "C#";
+            
+            for (int i = 0; i < contador; i++)
+            {
+                Console.WriteLine($"Ola {nome} Bem-Vindo ao Curso de {curso} {i +1} com a Digital Innovation One");
+            }
+
+        }
+    }
+}
